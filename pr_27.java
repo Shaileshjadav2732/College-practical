e38249df2217a4571235158d320ef528086934ca
Shailesh Jadav <113022776+Shaileshjadav2732@users.noreply.github.com>
2023-06-09 23:33:31 +0530

    Update pr_27.java

--- pr_27.java
+++ pr_27.java
@@ -5,8 +5,8 @@ class Circle {
 
     public Circle(double r) {
         radius = r;
-        x = 0.0;
-        y = 0.0;
+        x = 0;
+        y = 0;
     }
 
     public Circle(double x, double y, double r) {
@@ -18,11 +18,11 @@ class Circle {
 
 public class pr_27 {
     public static void main(String[] args) {
-        Circle circle1 = new Circle(5.0);
+        Circle circle1 = new Circle(5.2);
         System.out.println("Circle 1 radius: " + circle1.radius);
         System.out.println("Circle 1 center coordinates: (" + circle1.x + ", " + circle1.y + ")");
 
-        Circle circle2 = new Circle(1.0, 2.0, 3.0);
+        Circle circle2 = new Circle(1.5, 2.77, 3.4);
         System.out.println("Circle 2 radius: " + circle2.radius);
         System.out.println("Circle 2 center coordinates: (" + circle2.x + ", " + circle2.y + ")");
     }
